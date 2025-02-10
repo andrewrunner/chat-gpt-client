53cd6d68a9c5da2279c846408035af8f7a33e530
taranenko.a <taranenko.a@novapay.ua>
2025-02-10 17:18:44 +0200

    add react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './css/index.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router";
+
+import App from './pages/App.tsx'
+import SignInPage from "./pages/sign-in.page.tsx";
+import SignUpPage from "./pages/sign-up.page.tsx";
+import ChatPage from "./pages/chat.page.tsx";
+import NotFoundPage from "./pages/not-found.page.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Router>
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="sign-in" element={<SignInPage />} />
+              <Route path="sign-up" element={<SignUpPage />} />
+              <Route path="chat" element={<ChatPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+      </Router>
   </StrictMode>,
 )
---
+++ frontend/src/pages/not-found.page.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+    return (
+        <>
+            <h1>404</h1>
+            <h3>not found</h3>
+        </>
+    )
+}
+
+export default NotFoundPage
